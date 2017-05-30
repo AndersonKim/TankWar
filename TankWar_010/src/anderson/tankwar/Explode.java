cd08777c208afbf14d199b8c6f6ae214264225f8
AndersonKim <pgytao@outlook.com>
2017-05-30 23:35:53 +0800

    add words

--- TankWar_010/src/anderson/tankwar/Explode.java
+++ TankWar_010/src/anderson/tankwar/Explode.java
@@ -2,12 +2,21 @@ package anderson.tankwar;
 
 import java.awt.Color;
 import java.awt.Graphics;
-
+/**
+ * 爆炸类，在坦克受到攻击时产生
+ * @author lenovo
+ *
+ */
 public class Explode {
+	//爆炸坐标
 	int x,y;
+	//是否是活的爆炸，决定是否绘制
 	boolean live=true;
+	//设定爆炸的绘制半径
 	int[] diameter={4,7,12,18,28,32,49,30,14,6};
+	//确定当前进行到第几步的绘制
 	int step=0;
+	//持有的管理器的引用
 	TankClient tc;
 
 
@@ -20,16 +29,23 @@ public class Explode {
 
 
 
+	/**
+	 * 绘制爆炸
+	 * @param g 
+	 */
 	public void draw(Graphics g){
+		//去除已经死去的爆炸
 		if(!live) {
 			tc.explodes.remove(this);
 			return;	
 		}
+		//当爆炸进行到最后一步时，爆炸结束；计数归零
 		if(step==diameter.length){
 			live=false;
 			step=0;
 			return;
 		}
+		//取色暂存并且绘制爆炸，进行到下一步半径
 		Color c=g.getColor();
 		g.setColor(Color.ORANGE);
 		g.fillOval(x,y,diameter[step],diameter[step]);
--- TankWar_010/src/anderson/tankwar/Wall.java
+++ TankWar_010/src/anderson/tankwar/Wall.java
@@ -3,10 +3,17 @@ package anderson.tankwar;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
-
+/**
+ * 墙体类,坦克以及子弹无法穿越
+ * @author 	AndersonKim
+ * @mail	pgytao@outlook.com
+ */
 public class Wall {
+	//墙的位置以及宽度以及厚度
 	int x,y,w,h;
+	//持有的管理器的引用
 	TankClient tc;
+
 	public Wall(int x, int y, int w, int h, TankClient tc) {
 		super();
 		this.x = x;
@@ -15,13 +22,21 @@ public class Wall {
 		this.h = h;
 		this.tc = tc;
 	}
+	/**
+	 * 绘制墙体
+	 * @param g
+	 */
 	public void draw(Graphics g){
 		Color c=g.getColor();
 		g.setColor(Color.LIGHT_GRAY);
 		g.fillRect(x, y, w, h);
 		g.setColor(c);
 	}
-
+	
+	/**
+	 * 碰撞检测
+	 * @return
+	 */
 	public Rectangle getRect(){
 		return new Rectangle(x,y,w,h);
 	}
