b08f399dac25cd7073591ecf28a4f6d050b583eb
AndersonKim <pgytao@outlook.com>
2017-05-30 23:42:10 +0800

    add words

--- TankWar_010/src/anderson/tankwar/HealPack.java
+++ TankWar_010/src/anderson/tankwar/HealPack.java
@@ -3,10 +3,17 @@ package anderson.tankwar;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
-
+/**
+ * 血包类，我方坦克吃了可以回满血
+ * @author 	AndersonKim
+ * @mail	pgytao@outlook.com
+ */
 public class HealPack {
+	//血包的位置以及宽度以及厚度
 	int x,y,w,h;
+	//管理器的引用
 	TankClient tc;
+	//血包的可用性
 	boolean live=true;
 	//明确血包运动轨迹，使用二维数组构成
 	private int[][] path={
@@ -25,7 +32,11 @@ public class HealPack {
 		this.tc=tc;
 	}
 
+	//血包的绘制步骤
 	int step=0;
+	/**
+	 * 移动血包
+	 */
 	private void move(){
 		step++;
 		if(step==path.length){
@@ -34,10 +45,17 @@ public class HealPack {
 		x=path[step][0];
 		y=path[step][1];
 	}
+	/**
+	 * 碰撞检测
+	 * @return
+	 */
 	public Rectangle getRec(){
 		return new Rectangle(x,y,w,h);
 	}
-
+	/**
+	 * 绘制血包
+	 * @param g
+	 */
 	public void draw(Graphics g){
 		if(live){
 			Color c=g.getColor();
